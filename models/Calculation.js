d6f8c90fb498da5893078cd875ced6ee0f1bd971
HassanBahati <mukisabahati@gmail.com>
2021-07-14 17:56:56 +0300

    edited modal

--- models/Calculation.js
+++ models/Calculation.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const CalculationSchema = new mongoose.Schema({
-  //   username: {
-  //     type: String,
-  //     required: [true, "Please provide a username"],
-  //   },
   type: {
     type: String
   },
